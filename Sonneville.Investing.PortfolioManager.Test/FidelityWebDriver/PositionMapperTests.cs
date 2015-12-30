a3d9db379f1546e17bea97b55dad636e8c24f7b4
John Sonneville <SonnevilleJ@gmail.com>
2015-12-30 15:25:32 -0600

    Map position class.

---
+++ Sonneville.Investing.PortfolioManager.Test/FidelityWebDriver/PositionMapperTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NUnit.Framework;
+using Sonneville.Investing.PortfolioManager.FidelityWebDriver;
+using WebDriverPosition = Sonneville.FidelityWebDriver.Data.Position;
+
+namespace Sonneville.Investing.PortfolioManager.Test.FidelityWebDriver
+{
+    [TestFixture]
+    public class PositionMapperTests
+    {
+        [Test]
+        public void ShouldMapProperties()
+        {
+            var extractedPosition = new WebDriverPosition
+            {
+                Ticker = "a",
+                Quantity = 10,
+                LastPrice = 15,
+            };
+
+            var mappedPosition = new PositionMapper().Map(extractedPosition);
+
+            Assert.AreEqual("a", mappedPosition.Ticker);
+            Assert.AreEqual(DateTime.Today, mappedPosition.DateTime);
+            Assert.AreEqual(extractedPosition.Quantity, mappedPosition.Shares);
+            Assert.AreEqual(extractedPosition.LastPrice, mappedPosition.PerSharePrice);
+        }
+    }
+}
---
+++ Sonneville.Investing.PortfolioManager/FidelityWebDriver/PositionMapper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Sonneville.Investing.Trading;
+
+namespace Sonneville.Investing.PortfolioManager.FidelityWebDriver
+{
+    public class PositionMapper
+    {
+        public Position Map(Sonneville.FidelityWebDriver.Data.Position extractedPosition)
+        {
+            return new Position
+            {
+                DateTime = DateTime.Today,
+                Ticker = extractedPosition.Ticker,
+                Shares = extractedPosition.Quantity,
+                PerSharePrice = extractedPosition.LastPrice,
+            };
+        }
+    }
+}
