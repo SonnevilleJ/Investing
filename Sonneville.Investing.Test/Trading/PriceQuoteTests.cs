456b9ad5254ea0640af72ffc503827f19257384e
John Sonneville <SonnevilleJ@gmail.com>
2015-12-30 13:12:51 -0600

    Added PriceQuote.

---
+++ Sonneville.Investing.Test/Trading/PriceQuoteTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+using Sonneville.Investing.Trading;
+
+namespace Sonneville.Investing.Test.Trading
+{
+    [TestFixture]
+    public class PriceQuoteTests
+    {
+        [Test]
+        [TestCase("a", 5)]
+        public void ShouldReturnValuesSpecified(string ticker, int price)
+        {
+            var dateTime = DateTime.Today;
+            var priceQuote = new PriceQuote
+            {
+                Ticker = ticker,
+                PerSharePrice = price,
+                DateTime = dateTime
+            };
+
+            Assert.AreEqual(ticker, priceQuote.Ticker);
+            Assert.AreEqual(price, priceQuote.PerSharePrice);
+            Assert.AreEqual(dateTime, priceQuote.DateTime);
+        }
+    }
+}
---
+++ Sonneville.Investing/Trading/PriceQuote.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Sonneville.Investing.Trading
+{
+    public class PriceQuote
+    {
+        public string Ticker { get; set; }
+
+        public decimal PerSharePrice { get; set; }
+
+        public DateTime DateTime { get; set; }
+    }
+}
