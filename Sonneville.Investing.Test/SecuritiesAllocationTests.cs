79cc92df64fa62ce0e7efccf7f64fcf435275ab7
John Sonneville <SonnevilleJ@gmail.com>
2015-12-30 00:08:11 -0600

    Added SecuritiesAllocation.

---
+++ Sonneville.Investing.Test/SecuritiesAllocationTests.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Sonneville.Investing.Test
+{
+    [TestFixture]
+    public class SecuritiesAllocationTests
+    {
+        private Dictionary<string, double> _ratesByTicker;
+        private SecuritiesAllocation _securitiesAllocation;
+
+        [SetUp]
+        public void Setup()
+        {
+            _ratesByTicker = new Dictionary<string, double>
+            {
+                {"ticker1", 0.8 },
+                {"ticker2", 0.2 },
+            };
+            _securitiesAllocation = new SecuritiesAllocation(_ratesByTicker);
+        }
+
+        [Test]
+        public void ShouldReturnAmountSpecified()
+        {
+            foreach (var kvp in _ratesByTicker)
+            {
+                Assert.AreEqual(kvp.Value, _securitiesAllocation.GetAmount(kvp.Key));
+            }
+        }
+
+        [Test]
+        public void ShouldReturnZeroForUnknownTicker()
+        {
+            Assert.AreEqual(0, _securitiesAllocation.GetAmount("ticker3"));
+        }
+
+        [Test]
+        public void ShouldIterate()
+        {
+            foreach (var kvp in _securitiesAllocation)
+            {
+                Assert.AreEqual(_ratesByTicker[kvp.Key], kvp.Value);
+            }
+        }
+
+        [Test]
+        public void ShouldReturnEquivalentButNotSameDictionary()
+        {
+            var returnedDictionary = _securitiesAllocation.ToDictionary();
+
+            Assert.AreNotSame(_ratesByTicker, returnedDictionary);
+            CollectionAssert.AreEquivalent(_ratesByTicker, returnedDictionary);
+        }
+    }
+}
---
+++ Sonneville.Investing/SecuritiesAllocation.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Sonneville.Investing
+{
+    public interface ISecuritiesAllocation : IEnumerable<KeyValuePair<string, double>>
+    {
+    }
+
+    public class SecuritiesAllocation : ISecuritiesAllocation
+    {
+        private readonly Dictionary<string, double> _ratesByTicker;
+
+        public SecuritiesAllocation(Dictionary<string, double> ratesByTicker)
+        {
+            _ratesByTicker = ratesByTicker;
+        }
+
+        public double GetAmount(string ticker)
+        {
+            return _ratesByTicker.ContainsKey(ticker)
+                ? _ratesByTicker[ticker]
+                : 0;
+        }
+
+        public IReadOnlyDictionary<string, double> ToDictionary()
+        {
+            return new Dictionary<string, double>(_ratesByTicker);
+        }
+
+        public IEnumerator<KeyValuePair<string, double>> GetEnumerator()
+        {
+            return _ratesByTicker.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
