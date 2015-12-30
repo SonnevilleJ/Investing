663b331fdad59b27be567f2b556eabcb9d0095f8
John Sonneville <SonnevilleJ@gmail.com>
2015-12-30 13:04:33 -0600

    Added Position.

---
+++ Sonneville.Investing.Test/PositionTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Sonneville.Investing.Test
+{
+    [TestFixture]
+    public class PositionTests
+    {
+        [Test]
+        [TestCase("a", 5, 25)]
+        [TestCase("b", 1000000000, 1000000000)]
+        [TestCase("c", -1000000000, 1000000000)]
+        public void ShouldReturnValuesSpecified(string ticker, int shares, int perSharePrice)
+        {
+            var position = new Position
+            {
+                Ticker = ticker,
+                Shares = shares,
+                PerSharePrice = perSharePrice,
+                DateTime = DateTime.Today
+            };
+
+            Assert.AreEqual(ticker, position.Ticker);
+            Assert.AreEqual(shares, position.Shares);
+            Assert.AreEqual(perSharePrice, position.PerSharePrice);
+            Assert.AreEqual(DateTime.Today, position.DateTime);
+            Assert.AreEqual(shares*(decimal) perSharePrice, position.Value);
+        }
+    }
+}
---
+++ Sonneville.Investing/Position.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Sonneville.Investing
+{
+    public class Position
+    {
+        public string Ticker { get; set; }
+
+        public decimal Shares { get; set; }
+
+        public decimal PerSharePrice { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public decimal Value => Shares*PerSharePrice;
+    }
+}
