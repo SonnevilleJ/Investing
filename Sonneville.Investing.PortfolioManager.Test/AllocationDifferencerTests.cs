e23815a0e565e0f454ecc7744502b3924933f4a3
John Sonneville <SonnevilleJ@gmail.com>
2016-01-01 02:06:26 -0600

    Added AllocationDifferencer to measure the difference in allocations.

---
+++ Sonneville.Investing.PortfolioManager.Test/AllocationDifferencerTests.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using Sonneville.FidelityWebDriver.Data;
+
+namespace Sonneville.Investing.PortfolioManager.Test
+{
+    [TestFixture]
+    public class AllocationDifferencerTests
+    {
+        [Test]
+        public void ShouldDiffPercentages()
+        {
+            var minuend = new Dictionary<Position, decimal>
+            {
+                {new Position {Ticker = "a"}, 0.25m},
+                {new Position {Ticker = "b"}, 0.5m},
+                {new Position {Ticker = "c"}, 0.25m},
+            };
+            var subtrahend = new Dictionary<Position, decimal>
+            {
+                {new Position {Ticker = "a"}, 0.5m},
+                {new Position {Ticker = "b"}, 0.25m},
+                {new Position {Ticker = "c"}, 0.25m},
+            };
+
+            var difference = new AllocationDifferencer().CalculateDifference(minuend, subtrahend);
+
+            Assert.AreEqual(-0.25m, difference.Single(kvp => kvp.Key.Ticker == "a").Value);
+            Assert.AreEqual(0.25m, difference.Single(kvp => kvp.Key.Ticker == "b").Value);
+            Assert.AreEqual(0, difference.Single(kvp => kvp.Key.Ticker == "c").Value);
+        }
+
+        [Test]
+        public void ShouldIncludeExtraTickersFromMinuend()
+        {
+            var minuend = new Dictionary<Position, decimal>
+            {
+                {new Position {Ticker = "a"}, 0.25m},
+                {new Position {Ticker = "b"}, 0.5m},
+                {new Position {Ticker = "c"}, 0.25m},
+                {new Position {Ticker = "d"}, 0.25m},
+            };
+            var subtrahend = new Dictionary<Position, decimal>
+            {
+                {new Position {Ticker = "a"}, 0.5m},
+                {new Position {Ticker = "b"}, 0.25m},
+                {new Position {Ticker = "c"}, 0.25m},
+            };
+
+            var difference = new AllocationDifferencer().CalculateDifference(minuend, subtrahend);
+
+            Assert.AreEqual(-0.25m, difference.Single(kvp => kvp.Key.Ticker == "a").Value);
+            Assert.AreEqual(0.25m, difference.Single(kvp => kvp.Key.Ticker == "b").Value);
+            Assert.AreEqual(0, difference.Single(kvp => kvp.Key.Ticker == "c").Value);
+            Assert.AreEqual(0.25m, difference.Single(kvp => kvp.Key.Ticker == "d").Value);
+        }
+
+        [Test]
+        public void ShouldIncludeExtraTickersFromSubtrahend()
+        {
+            var minuend = new Dictionary<Position, decimal>
+            {
+                {new Position {Ticker = "a"}, 0.25m},
+                {new Position {Ticker = "b"}, 0.5m},
+                {new Position {Ticker = "c"}, 0.25m},
+            };
+            var subtrahend = new Dictionary<Position, decimal>
+            {
+                {new Position {Ticker = "a"}, 0.5m},
+                {new Position {Ticker = "b"}, 0.25m},
+                {new Position {Ticker = "c"}, 0.25m},
+                {new Position {Ticker = "d"}, 0.25m},
+            };
+
+            var difference = new AllocationDifferencer().CalculateDifference(minuend, subtrahend);
+
+            Assert.AreEqual(-0.25m, difference.Single(kvp => kvp.Key.Ticker == "a").Value);
+            Assert.AreEqual(0.25m, difference.Single(kvp => kvp.Key.Ticker == "b").Value);
+            Assert.AreEqual(0, difference.Single(kvp => kvp.Key.Ticker == "c").Value);
+            Assert.AreEqual(-0.25m, difference.Single(kvp => kvp.Key.Ticker == "d").Value);
+        }
+    }
+}
---
+++ Sonneville.Investing.PortfolioManager/AllocationDifferencer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Sonneville.FidelityWebDriver.Data;
+using Sonneville.Utilities.Extensions;
+
+namespace Sonneville.Investing.PortfolioManager
+{
+    public class AllocationDifferencer
+    {
+        public IDictionary<Position, decimal> CalculateDifference(IDictionary<Position, decimal> minuend,
+            IDictionary<Position, decimal> subtrahend)
+        {
+            return minuend.ZipAll(subtrahend,
+                (m, s) => new KeyValuePair<Position, decimal>(m.Key ?? s.Key, m.Value - s.Value))
+                .ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+        }
+    }
+}
