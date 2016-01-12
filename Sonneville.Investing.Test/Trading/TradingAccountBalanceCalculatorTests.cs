f94945c7c55de23286f9f3305c98e553938e3657
John Sonneville <SonnevilleJ@gmail.com>
2016-01-12 11:55:26 -0600

    Introduced TradingAccountBalanceCalculator.

---
+++ Sonneville.Investing.Test/Trading/TradingAccountBalanceCalculatorTests.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using Sonneville.Investing.Trading;
+
+namespace Sonneville.Investing.Test.Trading
+{
+    [TestFixture]
+    public class TradingAccountBalanceCalculatorTests
+    {
+        [Test]
+        public void ShouldSumSingleAccount()
+        {
+            var tradingAccount = new TradingAccount
+            {
+                Positions = new List<Position>
+                {
+                    new Position
+                    {
+                        Shares = 1,
+                        PerSharePrice = 10
+                    },
+                    new Position
+                    {
+                        Shares = 2,
+                        PerSharePrice = 3
+                    },
+                }
+            };
+
+            var calculator = new TradingAccountBalanceCalculator();
+
+            var balance = calculator.CalculateBalance(tradingAccount);
+
+            Assert.AreEqual(16, balance);
+        }
+
+        [Test]
+        public void ShouldSumMultipleAccounts()
+        {
+            var tradingAccounts = new List<TradingAccount>
+            {
+                new TradingAccount
+                {
+                    Positions = new List<Position>
+                    {
+                        new Position
+                        {
+                            Shares = 1,
+                            PerSharePrice = 10
+                        },
+                        new Position
+                        {
+                            Shares = 2,
+                            PerSharePrice = 3
+                        },
+                    }
+                },
+                new TradingAccount
+                {
+                    Positions = new List<Position>
+                    {
+                        new Position
+                        {
+                            Shares = 10,
+                            PerSharePrice = 5
+                        },
+                        new Position
+                        {
+                            Shares = 3,
+                            PerSharePrice = 7
+                        },
+                    }
+                }
+            };
+
+            var calculator = new TradingAccountBalanceCalculator();
+
+            var balance = calculator.CalculateBalance(tradingAccounts);
+
+            Assert.AreEqual(87, balance);
+        }
+    }
+}
---
+++ Sonneville.Investing/Trading/TradingAccountBalanceCalculator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Sonneville.Investing.Trading
+{
+    public class TradingAccountBalanceCalculator
+    {
+        public decimal CalculateBalance(TradingAccount tradingAccount)
+        {
+            return tradingAccount.Positions.Sum(position => position.Value);
+        }
+
+        public decimal CalculateBalance(IEnumerable<TradingAccount> tradingAccounts)
+        {
+            return tradingAccounts.Select(CalculateBalance).Sum();
+        }
+    }
+}
