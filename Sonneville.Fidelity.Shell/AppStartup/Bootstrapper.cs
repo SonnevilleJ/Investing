e14b04185d95f34ac4dee332fb0df200fa6e8fe4
John Sonneville <SonnevilleJ@gmail.com>
2016-07-04 13:48:01 -0500

    Removed setter from Kernel.

--- Sonneville.Fidelity.Shell/AppStartup/Bootstrapper.cs
+++ Sonneville.Fidelity.Shell/AppStartup/Bootstrapper.cs
@@ -4,12 +4,7 @@ namespace Sonneville.Fidelity.Shell.AppStartup
 {
     public class Bootstrapper
     {
-        public static IKernel Kernel { get; set; }
-
-        static Bootstrapper()
-        {
-            Kernel = new KernelBuilder().Build();
-        }
+        public static IKernel Kernel { get; } = new KernelBuilder().Build();
 
         public static void Main(string[] args)
         {
