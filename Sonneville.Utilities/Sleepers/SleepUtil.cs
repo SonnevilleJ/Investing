0a3e89ee3766f84cd63193eb13af9cd650e3d62b
John Sonneville <SonnevilleJ@gmail.com>
2017-02-21 21:32:32 -0600

    Add ability to sleep until an exit signal is received. Sleep namespace created.

---
+++ Sonneville.Utilities/Sleepers/SleepUtil.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Threading;
+
+namespace Sonneville.Utilities.Sleepers
+{
+    public interface ISleepUtil
+    {
+        void Sleep(int milliseconds);
+
+        void WaitForExitSignal();
+    }
+
+    public class SleepUtil : ISleepUtil
+    {
+        private readonly IUnixSignalWaiter _unixSignalWaiter;
+        private readonly TextReader _consoleIn;
+
+        public SleepUtil() : this(new UnixSignalWaiterWrapper(), Console.In)
+        {
+        }
+
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public SleepUtil(IUnixSignalWaiter unixSignalWaiter, TextReader textReader)
+        {
+            _unixSignalWaiter = unixSignalWaiter;
+            _consoleIn = textReader;
+        }
+
+        public void Sleep(int milliseconds)
+        {
+            Thread.Sleep(milliseconds);
+        }
+
+        public void WaitForExitSignal()
+        {
+            if (_unixSignalWaiter.CanWaitExitSignal())
+            {
+                _unixSignalWaiter.WaitExitSignal();
+            }
+            else
+            {
+                _consoleIn.ReadLine();
+            }
+        }
+    }
+}
