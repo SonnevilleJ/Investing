e7eb77ab1fbd189fca0b4df75fd3ffb4fd4da1e3
John Sonneville <SonnevilleJ@gmail.com>
2018-01-04 12:56:32 -0600

    Implemented freezable clock.

---
+++ Sonneville.Utilities.Test/FreezableClockTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+
+namespace Sonneville.Utilities.Test
+{
+    [TestFixture]
+    public class FreezableClockTests
+    {
+        [Test]
+        public void ShouldReturnActualTimeBeforeFrozen()
+        {
+            var clock = new FreezableClock();
+
+            Assert.AreEqual(DateTime.Today, clock.Now.Date);
+            Thread.Sleep(100);
+            Assert.AreEqual(DateTime.Today, clock.Now.Date);
+        }
+
+        [Test]
+        public void ShouldReturnSameTimeIfFrozen()
+        {
+            var clock = new FreezableClock();
+            var dateTime = new DateTime(2018, 1, 4, 9, 49, 13);
+            clock.Freeze(dateTime);
+
+            Assert.AreEqual(dateTime, clock.Now);
+            Thread.Sleep(100);
+            Assert.AreEqual(dateTime, clock.Now);
+        }
+        
+        [Test]
+        public void ShouldReturnActualTimeAfterFrozen()
+        {
+            var clock = new FreezableClock();
+            var time = new DateTime(2018, 1, 4, 9, 49, 13);
+            clock.Freeze(time);
+            clock.Unfreeze();
+
+            Assert.AreEqual(DateTime.Today, clock.Now.Date);
+            Thread.Sleep(100);
+            Assert.AreEqual(DateTime.Today, clock.Now.Date);
+        }
+    }
+}
---
+++ Sonneville.Utilities/FreezableClock.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Sonneville.Utilities
+{
+    public interface IClock
+    {
+        DateTime Now { get; }
+    }
+
+    public class FreezableClock : IClock
+    {
+        private DateTime? _now;
+
+        public void Freeze(DateTime dateTime)
+        {
+            Now = dateTime;
+        }
+
+        public DateTime Now
+        {
+            get => _now ?? DateTime.Now;
+            private set => _now = value;
+        }
+
+        public void Unfreeze()
+        {
+            _now = null;
+        }
+    }
+}
