6e055ecdc918366aed2428918da2aca61218971a
John Sonneville <SonnevilleJohn@JohnDeere.com>
2018-01-04 18:54:39 -0600

    Implemented SeleniumWaiter.

---
+++ Sonneville.Fidelity.Shell.Test/Logging/SeleniumWaiterTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Moq;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using Sonneville.Fidelity.Shell.Logging;
+
+namespace Sonneville.Fidelity.Shell.Test.Logging
+{
+    [TestFixture]
+    public class SeleniumWaiterTests
+    {
+        private Mock<IWebDriver> _webDriverMock;
+        private SeleniumWaiter _seleniumWaiter;
+        private TimeSpan _timeout;
+
+        [SetUp]
+        public void Setup()
+        {
+            _webDriverMock = new Mock<IWebDriver>();
+
+            _timeout = TimeSpan.FromMilliseconds(1000);
+
+            _seleniumWaiter = new SeleniumWaiter(_webDriverMock.Object);
+        }
+
+        [Test]
+        public void ShouldTimeoutAndThrowWhenConditionNeverMet()
+        {
+            var startTime = DateTime.Now;
+            Assert.Throws<WebDriverTimeoutException>(() => _seleniumWaiter.WaitUntil(_ => false, _timeout));
+            var endTime = DateTime.Now;
+
+            Assert.LessOrEqual(_timeout, endTime - startTime);
+        }
+        
+        [Test]
+        public void ShouldCompleteWhenConditionMet()
+        {
+            var startTime = DateTime.Now;
+            Assert.DoesNotThrow(() => _seleniumWaiter.WaitUntil(_ => true, _timeout));
+            var endTime = DateTime.Now;
+
+            Assert.GreaterOrEqual(_timeout, endTime - startTime);
+        }
+    }
+}
---
+++ Sonneville.Fidelity.Shell/Logging/SeleniumWaiter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace Sonneville.Fidelity.Shell.Logging
+{
+    public class SeleniumWaiter
+    {
+        private readonly IWebDriver _webDriver;
+
+        public SeleniumWaiter(IWebDriver webDriver)
+        {
+            _webDriver = webDriver;
+        }
+
+        public void WaitUntil(Func<IWebDriver, bool> condition, TimeSpan timeout)
+        {
+            new WebDriverWait(_webDriver, timeout)
+                .Until(condition);
+        }
+    }
+}
