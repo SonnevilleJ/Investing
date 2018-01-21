814c270b5cdf30cd3ec2bef17d877dec5fbbb07c
John Sonneville <SonnevilleJ@gmail.com>
2018-01-20 21:59:49 -0600

    Added automatic migration from v1 to v2 config.

--- Sonneville.Utilities.Test/Persistence/v2/JsonDataStoreTests.cs
+++ Sonneville.Utilities.Test/Persistence/v2/JsonDataStoreTests.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using Newtonsoft.Json.Linq;
 using NUnit.Framework;
+using Sonneville.Utilities.Persistence.v1;
 using Sonneville.Utilities.Persistence.v2;
 
 namespace Sonneville.Utilities.Test.Persistence.v2
@@ -28,9 +30,11 @@ namespace Sonneville.Utilities.Test.Persistence.v2
         {
             if (File.Exists(_path))
             {
-                Console.WriteLine($"Clearing persisted data... Contents follow: {Environment.NewLine}{File.ReadAllText(_path)}");
+                Console.WriteLine(
+                    $"Clearing persisted data... Contents follow: {Environment.NewLine}{File.ReadAllText(_path)}");
                 File.Delete(_path);
             }
+
             Assert.False(File.Exists(_path));
         }
 
@@ -304,6 +308,52 @@ namespace Sonneville.Utilities.Test.Persistence.v2
             Assert.AreEqual(otherData.A, retrievedOtherData.A);
         }
 
+        [Test]
+        public void LoadShouldUpgradeOldPersistedData()
+        {
+            var sampleData = CreateLegacySampleData();
+
+            var legacyConfigStore = new JsonConfigStore(_path);
+            legacyConfigStore.Save(sampleData);
+            Assert.AreEqual(sampleData.A, legacyConfigStore.Get<SampleData>().A);
+            
+            var retrievedSampleData = _store.Get<SampleData>();
+            Assert.AreEqual(sampleData.A, retrievedSampleData.A);
+            Assert.AreEqual(sampleData.B, retrievedSampleData.B);
+            Assert.AreEqual(sampleData.C, retrievedSampleData.C);
+            Assert.AreEqual(sampleData.D, retrievedSampleData.D);
+            Assert.AreEqual(sampleData.E, retrievedSampleData.E);
+            Assert.AreEqual(sampleData.F, retrievedSampleData.F);
+            Assert.AreEqual(sampleData.G, retrievedSampleData.G);
+            Assert.AreEqual(sampleData.H, retrievedSampleData.H);
+            Assert.AreEqual(sampleData.I, retrievedSampleData.I);
+        }
+
+        [Test]
+        public void LoadShouldThrowIfNewerVersionDetected()
+        {
+            var jObject = JObject.FromObject(new {Version = "v6"});
+            File.WriteAllText(_path, jObject.ToString());
+
+            Assert.Throws<NotSupportedException>(() => _store.Load<SampleData>());
+        }
+
+        private static SampleData CreateLegacySampleData()
+        {
+            return new SampleData
+            {
+                A = "sample",
+                B = 5,
+                C = 500,
+                D = 1234.56,
+                E = 123.4567m,
+                F = new TimeSpan(5, 1, 2, 3),
+                G = new HashSet<string>(new[] {"backward compatibility FTW"}),
+                H = new Dictionary<Type, object> {{typeof(string), "hi!"}},
+                I = new List<string> {"one", "two", "three"},
+            };
+        }
+
         private class SampleData
         {
             public string A { get; set; }
@@ -319,9 +369,9 @@ namespace Sonneville.Utilities.Test.Persistence.v2
             public TimeSpan F { get; set; }
 
             public HashSet<string> G { get; set; }
-            
+
             public Dictionary<Type, object> H { get; set; }
-            
+
             public List<string> I { get; set; }
         }
 
--- Sonneville.Utilities/Persistence/v2/JsonDataStore.cs
+++ Sonneville.Utilities/Persistence/v2/JsonDataStore.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.IO;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Sonneville.Utilities.Persistence.v1;
 
 namespace Sonneville.Utilities.Persistence.v2
 {
     public class JsonDataStore : IDataStore
     {
+        private const string DataStoreVersion = "v2";
         private readonly string _path;
         private readonly JsonMule _jsonMule;
 
@@ -44,33 +47,53 @@ namespace Sonneville.Utilities.Persistence.v2
 
         public T Load<T>() where T : class, new()
         {
-            Console.WriteLine($"Trying to load {typeof(T)}");
             var configFromCache = ReadFromCacheOr(() => new T());
 
             if (File.Exists(_path))
             {
-                var jsonFile = File.ReadAllText(_path);
-                var jsonMule = JsonConvert.DeserializeObject<JsonMule>(jsonFile, JsonSerialization.Settings);
-                foreach (var propertyInfo in typeof(T).GetProperties())
+                var json = File.ReadAllText(_path);
+                var jObject = JObject.Parse(json);
+                var serializedVersion = jObject[nameof(JsonMule.Version)]?.Value<string>();
+                if (serializedVersion == null)
                 {
-                    var jsonMuleObject = jsonMule.Cache[typeof(T)];
-                    var result = JsonConvert.DeserializeObject<T>(jsonMuleObject.ToString());
-                    propertyInfo.SetValue(configFromCache, propertyInfo.GetValue(result));
+                    var oldT = new JsonConfigStore(_path).Load<T>();
+                    Merge(configFromCache, oldT);
+                }
+                else if (serializedVersion == DataStoreVersion)
+                {
+                    var jsonSerializer = JsonSerializer.Create(JsonSerialization.Settings);
+                    var jsonMule = jObject.ToObject<JsonMule>(jsonSerializer);
+                    var result = JsonConvert.DeserializeObject<T>(jsonMule.Cache[typeof(T)].ToString());
+                    Merge(configFromCache, result);
+                }
+                else
+                {
+                    throw new NotSupportedException($"Unable to deserialize version {serializedVersion}!");
                 }
             }
 
             return configFromCache;
         }
 
-        private T ReadFromCacheOr<T>(Func<T> instantiator) where T : class, new()
+        private static void Merge<T>(T configFromCache, T result) where T : class, new()
+        {
+            foreach (var propertyInfo in typeof(T).GetProperties())
+            {
+                propertyInfo.SetValue(configFromCache, propertyInfo.GetValue(result));
+            }
+        }
+
+        private T ReadFromCacheOr<T>(Func<T> supplier) where T : class, new()
         {
             return _jsonMule.Cache.TryGetValue(typeof(T), out var existing)
                 ? existing as T
-                : instantiator();
+                : supplier();
         }
 
         private class JsonMule
         {
+            public readonly string Version = DataStoreVersion;
+
             public readonly Dictionary<Type, object> Cache = new Dictionary<Type, object>();
         }
     }
