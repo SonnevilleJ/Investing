d2601861f11d8ecfb95a6f719db5fd6c7f778e82
John Sonneville <SonnevilleJ@gmail.com>
2018-02-11 03:01:01 -0600

    Completed in-memory data store.

---
+++ Sonneville.Utilities.Test/Persistence/v2/InMemoryDataStoreIntegrationTests.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using NUnit.Framework;
+using Sonneville.Utilities.Persistence.v2;
+
+namespace Sonneville.Utilities.Test.Persistence.v2
+{
+    [TestFixture]
+    public class InMemoryDataStoreIntegrationTests
+    {
+        private InMemoryDataStore _store;
+
+        [SetUp]
+        public void Setup()
+        {
+            _store = new InMemoryDataStore();
+            _store.DeleteAll();
+        }
+
+        [Test]
+        public void GetShouldLoadIfNotCached()
+        {
+            _store.Save(new SampleData
+            {
+                A = "original",
+            });
+
+            var configStore = new InMemoryDataStore();
+            var one = configStore.Get<SampleData>();
+            var two = configStore.Get<SampleData>();
+
+            Assert.AreEqual("original", one.A);
+            Assert.AreEqual("original", two.A);
+            Assert.AreSame(one, two);
+        }
+
+        [Test]
+        public void GetShouldNotLoadAndReturnCachedIfCached()
+        {
+            var data = new SampleData
+            {
+                A = "original",
+            };
+            _store.Save(data);
+            data.A = "changed";
+
+            var result = _store.Get<SampleData>();
+            Assert.AreEqual("changed", result.A);
+            Assert.AreSame(data, result);
+        }
+
+        [Test]
+        public void GetShouldReturnDefaultIfNotPersisted()
+        {
+            _store.Save(new SampleData
+            {
+                A = "original",
+            });
+
+            var configStore = new InMemoryDataStore();
+            var data = configStore.Get<OtherData>();
+
+            AssertDefaultConfig(data);
+        }
+
+        [Test]
+        public void LoadShouldReturnDefaultIfNotPersisted()
+        {
+            _store.Save(new SampleData
+            {
+                A = "original",
+            });
+
+            var configStore = new InMemoryDataStore();
+            var data = configStore.Load<OtherData>();
+
+            AssertDefaultConfig(data);
+        }
+
+        [Test]
+        public void LoadShouldUpdateAndReturnPersistedConfig()
+        {
+            var data = new SampleData
+            {
+                A = "original",
+            };
+            _store.Save(data);
+            data.A = "changed";
+
+            var result = _store.Load<SampleData>();
+            Assert.AreEqual("original", result.A);
+            Assert.AreSame(data, result);
+        }
+
+        [Test]
+        public void LoadShouldReturnDefaultConfigdWhenNoFilePresent()
+        {
+            var data = _store.Load<SampleData>();
+
+            AssertDefaultConfig(data);
+        }
+
+        [Test]
+        public void DeleteAllShouldEmptyCache()
+        {
+            var data = new SampleData
+            {
+                A = "original",
+            };
+            _store.Save(data);
+
+            _store.DeleteAll();
+
+            AssertDefaultConfig(_store.Get<SampleData>());
+        }
+
+        [Test]
+        public void SaveShouldThrowIfSavingDifferentConfigOfSameType()
+        {
+            _store.Get<SampleData>();
+
+            var imposter = new SampleData();
+            Assert.Throws<ArgumentOutOfRangeException>(() => _store.Save(imposter));
+        }
+
+        private static void AssertDefaultConfig<T>(T data)
+        {
+            Assert.NotNull(data);
+            foreach (var propertyInfo in typeof(T).GetProperties())
+            {
+                var propertyType = propertyInfo.PropertyType;
+                var defaultValue = propertyType.IsValueType ? Activator.CreateInstance(propertyType) : null;
+                var actualValue = propertyInfo.GetValue(data);
+                Assert.AreEqual(defaultValue, actualValue);
+            }
+        }
+    }
+}
---
+++ Sonneville.Utilities/Persistence/v2/InMemoryDataStore.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Sonneville.Utilities.Persistence.v2
+{
+    public class InMemoryDataStore : IDataStore
+    {
+        private static Dictionary<Type, object> _persistedCache;
+        private Dictionary<Type, object> _inMemoryCache = new Dictionary<Type, object>();
+
+        public InMemoryDataStore()
+        {
+            if (_persistedCache == null)
+            {
+                DeleteAll();
+            }
+        }
+
+        public T Get<T>() where T : class, new()
+        {
+            return ReadFromMemoryOr(Load<T>);
+        }
+
+        public void Save<T>(T config)
+        {
+            var type = typeof(T);
+            if (_inMemoryCache.TryGetValue(type, out var existing) && !ReferenceEquals(existing, config))
+            {
+                throw new ArgumentOutOfRangeException(nameof(config), config, "Must pass original config instance!");
+            }
+            _inMemoryCache[type] = config;
+            
+            var json = JsonConvert.SerializeObject(config, JsonSerialization.Settings);
+            var jObject = JObject.Parse(json);
+            var jsonSerializer = JsonSerializer.Create(JsonSerialization.Settings);
+            var clone = jObject.ToObject<T>(jsonSerializer);
+
+            _persistedCache[type] = clone;
+        }
+
+        public T Load<T>() where T : class, new()
+        {
+            var configFromMemory = ReadFromMemoryOr(() => new T());
+            var configFromPersisted = _persistedCache.TryGetValue(typeof(T), out var retrieved) ? retrieved as T : configFromMemory;
+            return Merge(configFromMemory, configFromPersisted);
+        }
+
+        public void DeleteAll()
+        {
+            _persistedCache = new Dictionary<Type, object>();
+            _inMemoryCache = new Dictionary<Type, object>();
+        }
+
+        private T ReadFromMemoryOr<T>(Func<T> supplier) where T : class, new()
+        {
+            var type = typeof(T);
+            if (!_inMemoryCache.ContainsKey(type))
+            {
+                _inMemoryCache[type] = supplier();
+            }
+
+            return _inMemoryCache[type] as T;
+        }
+
+        private static T Merge<T>(T configFromMemory, T result) where T : class, new()
+        {
+            foreach (var propertyInfo in typeof(T).GetProperties())
+            {
+                propertyInfo.SetValue(configFromMemory, propertyInfo.GetValue(result));
+            }
+
+            return configFromMemory;
+        }
+    }
+}
