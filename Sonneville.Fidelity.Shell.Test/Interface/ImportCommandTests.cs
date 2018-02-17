7e00d6d9a14c26b5d3437b3a4f59eaa7c5548782
John Sonneville <SonnevilleJohn@JohnDeere.com>
2018-02-17 16:09:32 -0600

    Added ImportCommand to import CSV transactions

---
+++ Sonneville.Fidelity.Shell.Test/Interface/ImportCommandTests.cs
@@ -0,0 +1,122 @@
+using System.Collections.Generic;
+using System.IO;
+using Moq;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+using Sonneville.Fidelity.Shell.Interface;
+using Sonneville.Fidelity.WebDriver.Data;
+using Sonneville.Fidelity.WebDriver.Transactions.CSV;
+using Sonneville.Investing.Persistence;
+
+namespace Sonneville.Fidelity.Shell.Test.Interface
+{
+    [TestFixture]
+    public class ImportCommandTests
+    {
+        private StreamReader _inputReader;
+        private StreamWriter _inputWriter;
+        private StreamReader _outputReader;
+        private StreamWriter _outputWriter;
+        private string _filePath;
+        private Mock<ITransactionsMapper> _transactionsMapperMock;
+        private ImportCommand _importCommand;
+        private string _fileContents;
+        private List<IFidelityTransaction> _fidelityTransactions;
+        private Mock<ITransactionRepository> _transactionRepository;
+
+        [SetUp]
+        public void Setup()
+        {
+            _filePath = Path.GetTempFileName();
+
+            _fileContents = "file contents";
+            File.WriteAllText(_filePath, _fileContents);
+
+            var inputStream = new MemoryStream();
+            _inputReader = new StreamReader(inputStream);
+            _inputWriter = new StreamWriter(inputStream) {AutoFlush = true};
+
+            var outputStream = new MemoryStream();
+            _outputReader = new StreamReader(outputStream);
+            _outputWriter = new StreamWriter(outputStream) {AutoFlush = true};
+
+            _fidelityTransactions = new List<IFidelityTransaction>
+            {
+                new FidelityTransaction()
+            };
+            
+            _transactionsMapperMock = new Mock<ITransactionsMapper>();
+            _transactionsMapperMock.Setup(transactionMapper => transactionMapper.ParseCsv(_fileContents))
+                .Returns(_fidelityTransactions);
+
+            _transactionRepository = new Mock<ITransactionRepository>();
+
+            _importCommand = new ImportCommand(_transactionsMapperMock.Object, _transactionRepository.Object);
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            if (File.Exists(_filePath)) File.Delete(_filePath);
+        }
+
+        [Test]
+        public void ShouldBeNamedImport()
+        {
+            Assert.AreEqual("import", _importCommand.CommandName);
+        }
+
+        [Test]
+        public void ShouldPassFileContentsToTransactionsMapper()
+        {
+            var fullInput = $"import {_filePath}".Split(' ');
+            var shouldExit = InvokeWithConsole(fullInput);
+
+            Assert.IsFalse(shouldExit);
+            _transactionRepository.Verify(repository =>
+                repository.Save(
+                    It.Is<IEnumerable<IFidelityTransaction>>(transactions => AssertTransactionsMatch(transactions))));
+        }
+
+        [Test]
+        public void ShouldThrowIfFileDoesNotExist()
+        {
+            File.Delete(_filePath);
+
+            var fullInput = $"import {_filePath}".Split(' ');
+            Assert.Throws<FileNotFoundException>(() => InvokeWithConsole(fullInput));
+        }
+
+        [Test]
+        public void ShouldPrintHelpIfMissingFileNameArgument()
+        {
+            var fullInput = "import".Split(' ');
+            var shouldExit = InvokeWithConsole(fullInput);
+
+            Assert.IsFalse(shouldExit);
+            AssertOutputContains("Usage:");
+        }
+
+        private bool InvokeWithConsole(IReadOnlyList<string> fullInput)
+        {
+            return _importCommand.Invoke(_inputReader, _outputWriter, fullInput);
+        }
+
+        private void AssertOutputContains(string value)
+        {
+            Assert.That(ReadOutputText(), new ContainsConstraint(value));
+        }
+
+        private string ReadOutputText()
+        {
+            _outputReader.BaseStream.Position = 0;
+            return _outputReader.ReadToEnd();
+        }
+
+        private bool AssertTransactionsMatch(IEnumerable<IFidelityTransaction> transactions)
+        {
+            CollectionAssert.AreEquivalent(_fidelityTransactions, transactions);
+            return true;
+        }
+    }
+}
---
+++ Sonneville.Fidelity.Shell/Interface/ImportCommand.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+using System.IO;
+using Sonneville.Fidelity.WebDriver.Transactions.CSV;
+using Sonneville.Investing.Persistence;
+
+namespace Sonneville.Fidelity.Shell.Interface
+{
+    public class ImportCommand : ICommand
+    {
+        private readonly ITransactionsMapper _transactionsMapper;
+        private readonly ITransactionRepository _transactionRepository;
+
+        public ImportCommand(ITransactionsMapper transactionsMapper, ITransactionRepository transactionRepository)
+        {
+            _transactionsMapper = transactionsMapper;
+            _transactionRepository = transactionRepository;
+        }
+
+        public string CommandName => "import";
+
+        public bool Invoke(TextReader inputReader, TextWriter outputWriter, IReadOnlyList<string> fullInput)
+        {
+            if (fullInput.Count >= 2)
+            {
+                var filePath = fullInput[1];
+                var csvContent = File.ReadAllText(filePath);
+                outputWriter.WriteLine(csvContent);
+                var transactions = _transactionsMapper.ParseCsv(csvContent);
+                _transactionRepository.Save(transactions);
+
+                return false;
+            }
+
+            PrintUsage(outputWriter);
+            return false;
+        }
+
+        private void PrintUsage(TextWriter outputWriter)
+        {
+            outputWriter.WriteLine("Usage:");
+            outputWriter.WriteLine($"{CommandName} [FILE]");
+            outputWriter.WriteLine("Parse transactions from a FILE.");
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
